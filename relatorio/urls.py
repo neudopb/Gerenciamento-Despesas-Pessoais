5aadad752b7891864063eb32baa1bc6bbb81168d
neudinho11 <neudinho66@gmail.com>
2021-03-15 16:09:30 -0300

    Relatorios personalizados

--- relatorio/urls.py
+++ relatorio/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import GeneratePdf
+from .views import GeneratePDFGeral, GeneratePDFReceitas, GeneratePDFDespesas
 
 app_name = 'relatorio'
 
 urlpatterns = [
-    path('relatorio/', GeneratePdf.as_view(), name='gerar_relatorio'),
+    path('relatorio-geral/', GeneratePDFGeral.as_view(), name='gerar_relatorio_geral'),
+    path('relatorio-receitas/', GeneratePDFReceitas.as_view(), name='gerar_relatorio_receitas'),
+    path('relatorio-despesas/', GeneratePDFDespesas.as_view(), name='gerar_relatorio_despesas'),
 ]
--- relatorio/views.py
+++ relatorio/views.py
@@ -2,21 +2,66 @@ from django.http import HttpResponse
 from django.views.generic import View
 from despesasPessoais.utils import render_to_pdf
 from django.template.loader import get_template
-from gerenciador.models import Receita, Gerenciador
+from gerenciador.models import Receita, Gerenciador, Despesa
 
-class GeneratePdf(View):
+class GeneratePDFGeral(View):
     def get(self, request, *args, **kwargs):
 
         gerenc = Gerenciador.objects.get(id_usuario=self.request.user.id)
-        receita = Receita.objects.order_by('data', 'id_categoria', ).filter(id_gerenciador=gerenc.id)
-        data = {'receitas': receita}
+        receita = Receita.objects.order_by('data', 'id_categoria',).filter(id_gerenciador=gerenc.id)
+        despesa = Despesa.objects.order_by('data', 'id_categoria',).filter(id_gerenciador=gerenc.id)
+        data = {'receitas': receita, 'despesas': despesa, 'gerenciador': gerenc}
 
         #html = template.render(data)
         pdf = render_to_pdf('relatorio/invoice.html', data)
 
         if pdf:
             response = HttpResponse(pdf, content_type='application/pdf')
-            filename = "Relatorio_Receita_%s.pdf" %(gerenc.id)
+            filename = "Relatorio_Geral.pdf"
+            #content = "inline; filename='%s'" %(filename)
+            #download = request.GET.get("download")
+            #if download:
+            content = 'attachment; filename=%s' %(filename)
+            response['Content-Disposition'] = content
+            return response
+        else:
+            return HttpResponse("Not found")
+
+class GeneratePDFReceitas(View):
+    def get(self, request, *args, **kwargs):
+
+        gerenc = Gerenciador.objects.get(id_usuario=self.request.user.id)
+        receita = Receita.objects.order_by('data', 'id_categoria',).filter(id_gerenciador=gerenc.id)
+        data = {'receitas': receita, 'gerenciador': gerenc}
+
+        #html = template.render(data)
+        pdf = render_to_pdf('relatorio/invoiceReceita.html', data)
+
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Relatorio_Receitas.pdf"
+            #content = "inline; filename='%s'" %(filename)
+            #download = request.GET.get("download")
+            #if download:
+            content = 'attachment; filename=%s' %(filename)
+            response['Content-Disposition'] = content
+            return response
+        else:
+            return HttpResponse("Not found")
+
+class GeneratePDFDespesas(View):
+    def get(self, request, *args, **kwargs):
+
+        gerenc = Gerenciador.objects.get(id_usuario=self.request.user.id)
+        despesa = Despesa.objects.order_by('data', 'id_categoria',).filter(id_gerenciador=gerenc.id)
+        data = {'despesas': despesa, 'gerenciador': gerenc}
+
+        #html = template.render(data)
+        pdf = render_to_pdf('relatorio/invoiceDespesa.html', data)
+
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Relatorio_Despesas.pdf"
             #content = "inline; filename='%s'" %(filename)
             #download = request.GET.get("download")
             #if download:
